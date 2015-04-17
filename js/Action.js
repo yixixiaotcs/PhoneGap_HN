f83c63295ff0cd887771f0e4250f8ba8b84b8bf3
Frank <yixi.xiao@tcs.com>
2015-04-17 11:12:55 -0400

    ShowMore function added

--- js/Action.js
+++ js/Action.js
@@ -18,52 +18,123 @@ function onload(){
 	for (i = CURRENT_DISPLAY_STORIES_AMT; i<TOTAL_DISPLAY_STORIES; i++){
 		div.innerHTML = div.innerHTML 
 			+ "<div class='story'>"
-				+ "<div class='storyContent storyTT'></div>"
+				+ "<div class='storyContent storyTT'><div class='storyTTText'></div></div>"
 				+ "<div class='storyContent storyFN'>"
 					
 				+ "</div>"
-				+ "<div class='storyContent storyPV'>"
+				+ "<div class='storyContent storyPV' url='' onclick='' style=''>"
 					
 				+ "</div>"
 			+ "</div>";
 	}
-	testBTFV();
-	setStoryID(result);
+	
+	setStory(result);
+	
 }
 
-
-
-function testBTRV(){
-	window.location.href="https://www.google.com";
+// function to redirect to URL
+function redirect(url){
+	//var story = document.getElementById("pv"+id);
+	window.open(url);
 }
 
-function testBTFV(){
-	console.log("testing story titles loaded.");
-}
-
-
-function setStoryID(result){
+// add unique id to story divs and their inner divs.
+function setStory(result){
+	// pull all of the stories and components divs
 	var stories = document.getElementsByClassName("story");
 	var titles = document.getElementsByClassName("storyContent storyTT");
+	var titleContents = document.getElementsByClassName("storyTTText");
 	var functions = document.getElementsByClassName("storyContent storyFN");
 	var previews = document.getElementsByClassName("storyContent storyPV");
+	
 	for(i = CURRENT_DISPLAY_STORIES_AMT; i<TOTAL_DISPLAY_STORIES; i++){
 		if(stories[i].id.length == 0){ // check if the story iv already have a story attached
+			var t = i-CURRENT_DISPLAY_STORIES_AMT;
+			
 			// set the main story wrapper div id
-			stories[i].id = result[i].id;
+			stories[i].id = 'st' + result[t].id;
+			
 			// set the story title div id
-			titles[i].id = result[i].id;
-			// set the story button div id
-			functions[i].id = result[i].id;
-			// set the story preview div id
-			previews[i].id = result[i].id;
+			titles[i].id = 'tt' + result[t].id;
+			titles[i].setAttribute("url",result[t].url);
 			
 			// set the story title content
-			titles[i].innerHTML += result[i].title;
+			titleContents[i].id = 'ttct' + result[t].id;
+			titleContents[i].innerHTML += result[t].title;
+			
+			// set the story button div id
+			functions[i].id = 'rs' + result[t].id;
+			
+			// set the story preview div id
+			previews[i].id = 'pv' + result[t].id;
+			// set the preview's url
+			previews[i].setAttribute("url",result[t].url);
+			previews[i].setAttribute("onclick", "redirect(this.getAttribute('url'))");
+			
+			titles[i].setAttribute("onclick", "togglePreview(this.id)");
+			
 		}
 	}
-	CURRENT_DISPLAY_STORIES_AMT = TOTAL_DISPLAY_STORIES;
+	
+	CURRENT_DISPLAY_STORIES_AMT = TOTAL_DISPLAY_STORIES;	
 }
+
+
+/*
+	add new divs into the mainDisplayDiv
+*/
+function appendDiv(){
+    TOTAL_DISPLAY_STORIES = TOTAL_DISPLAY_STORIES + INCREMENT;
+	
+	var div = document.getElementById('mainDisplayDiv');
+	
+	for (i = CURRENT_DISPLAY_STORIES_AMT; i<TOTAL_DISPLAY_STORIES; i++){
+		div.innerHTML = div.innerHTML 
+			+ "<div class='story'>"
+				+ "<div class='storyContent storyTT'>"
+					+ "<div class='storyTTText'></div>"
+				+ "</div>"
+				+ "<div class='storyContent storyFN'>"
+					
+				+ "</div>"
+				+ "<div class='storyContent storyPV' url='' onclick='' style=''>"
+					
+				+ "</div>"
+			+ "</div>";
+	}
+	
+}
+
+/* 
+	showmore button function.
+	1. add new empty divs
+	2. fill content in
+*/
+function showMore() {
+	appendDiv();
+	var result = getHYTopStories();
+	
+	setStory(result);
+	
+}
+
+
+
+// toggle visibility of a preview
+function togglePreview(id) {
+	
+	var pvid='pv'+id.substring(2,9);
+	var pv = document.getElementById(pvid);
+	
+	
+	$("#" + pvid).animate({
+			height: 'toggle',
+			margin: 'toggle'
+	}, 100);
+
+}
+
+
 
 
 function HY_GET_STORY_BY_ID(id) { // get story by id
@@ -96,3 +167,73 @@ function getHYStoryByID(id) {
     xmlHttp.send(null);
     return xmlHttp.responseText;
 } // getHYSTORyByID
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Content pulling block %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function getLink(url, callBack) {
+	var q = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + url + '" and xpath="//title|//head/meta"') + '&format=json&callback=?';
+	
+	//console.log(q);
+	getContent(url, q, function(text)
+	{
+		callBack(text);
+	});
+}
+
+function getContent(url, q, callBack){
+	var text ='placeholder';
+	$.ajax({
+		type: 'GET',
+		url: q,
+		dataType: 'jsonp',
+		success: function(data, textStatus) {
+			// make sure we have some data to work with
+			if (results) {
+
+				// will contain only the data we want to display
+				var result = {};
+
+				// use the html title as the title
+				result.title = results.title;
+
+				// loop through meta tags to grab the image & description
+				$.each(data.query.results.meta, function(key, val) {
+					if (val.content) {
+						// description
+						if (val.name && val.name == 'description') {
+							result.description = val.content;
+						}
+						
+						
+
+						// og:image (Facebook image) -- starts with http:// and has a . 4 characters from the end
+						if (val.content.substring(0, 7) == 'http://' && val.content.charAt(val.content.length - 4) == '.') {
+							if (val.content != 'undefined') {
+								result.img = val.content;
+							}
+						}
+					}
+				});
+				// build output
+				text = '<div><h1>' + result.title + '</h1>';
+				text += result.img ? '<img src="' + result.img + '" class="thumb"/>' : '';
+				text += '<p>' + result.description + '</p></div>';
+				
+			callBack(text);
+			}
+		}
+	});
+}
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Content pulling block %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
